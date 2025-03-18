576927450b57afa6c74380a25646741363f427a0
xiaocheng sun <apple.splandy@gmail.com>
2025-03-18 02:59:37 +0000

    added swagger ui with documentation for spring main class

--- accounts/src/main/java/com/microservices/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/microservices/accounts/AccountsApplication.java
@@ -1,11 +1,33 @@
 package com.microservices.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Banking Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Shawn Sun",
+						email = "apple.splandy@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Banking Accounts microservice REST API Documentation"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
