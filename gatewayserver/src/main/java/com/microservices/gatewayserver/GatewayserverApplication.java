33a04dfc0c461cf29ba91aab1abff8fef6f9dca5
xiaocheng sun <apple.splandy@gmail.com>
2025-04-14 23:23:06 +0000

    Redis rateLimiter in gateway server
    Signed-off-by: xiaocheng sun <apple.splandy@gmail.com>

--- gatewayserver/src/main/java/com/microservices/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/microservices/gatewayserver/GatewayserverApplication.java
@@ -7,10 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -42,7 +45,9 @@ public class GatewayserverApplication {
 				.route(p -> p
 						.path("/bank/cards/**")
 						.filters( f -> f.rewritePath("/bank/cards/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+								.setKeyResolver(userKeyResolver())))
 						.uri("lb://CARDS")).build();
 
 
@@ -59,5 +64,25 @@ public class GatewayserverApplication {
 								.timeoutDuration(Duration.ofSeconds(4)) // Custom timeout duration
 								.build()
 				).build());
+	}
+
+	// Define a bean for RedisRateLimiter
+	@Bean
+	public RedisRateLimiter redisRateLimiter() {
+		// Create a new RedisRateLimiter with the following settings:
+		// replenishRate: 1 - tokens added per second
+		// burstCapacity: 1 - maximum number of tokens that can be stored
+		// requestedTokens: 1 - number of tokens needed per request
+		return new RedisRateLimiter(1, 1, 1);
+	}
+
+	// Define a KeyResolver bean to extract the user key from the request headers
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange ->
+				// Try to extract the value of the "user" header from the incoming request
+				Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+						// If the "user" header is missing, default to "anonymous"
+						.defaultIfEmpty("anonymous");
 	}
 }
